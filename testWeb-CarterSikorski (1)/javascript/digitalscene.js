613d577695fdf2934a254df49fc4271794bce161
sikorskicarter <54942508+sikorskicarter@users.noreply.github.com>
2020-12-22 15:15:05 -0600

    Add files via upload

---
+++ testWeb-CarterSikorski (1)/javascript/digitalscene.js
@@ -0,0 +1,26 @@
+function setup() {
+	var canvas=createCanvas(500, 500);
+	background(102,178,255);
+	
+}
+function draw() {
+	fill(0,153,0);
+	rect(0,400,500,100);
+	fill(255,255,0);
+	ellipse(50,50,65,65);
+	fill(224,224,224);
+	rect(100,250,200,150);
+	fill(224,224,224);
+	rect(125,190,40,50);
+	fill(255,0,0);
+	triangle(75, 250, 200, 200, 325, 250);
+	fill(0,0,0);
+	rect(125,260,40,40);
+	fill(0,0,0);
+	rect(235,260,40,40);
+	fill(102,51,0);
+	rect(182,350, 35, 50,);
+	textSize(18);
+	fill(0,0,0);
+	text("ACSCompSciPandemic2020 Carter S", 25,150);
+}
